2b83c27851d335c2a6a229d31184b9bf1805773f
Wyatt bier <bierw1@udayton.edu>
2025-11-23 18:00:59 -0500

    fix: days for countdown

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -17,13 +17,13 @@ function formatDate(value) {
 function formatCountdown(value = 0) {
   var seconds = parseInt((value / 1000) % 60),
     minutes = parseInt((value / (1000 * 60)) % 60),
-    hours = parseInt((value / (1000 * 60 * 60)) % 24)
-    days = parseInt((value / (1000 * 60 * 60 * 24)))
+    hours = parseInt((value / (1000 * 60 * 60)) % 24),
+    days = parseInt(value / (1000 * 60 * 60 * 24))
   hours = hours < 10 ? '0' + hours : hours
   minutes = minutes < 10 ? '0' + minutes : minutes
   seconds = seconds < 10 ? '0' + seconds : seconds
-  
-  return days + " Days, " + hours + ':' + minutes + ':' + seconds
+
+  return days + ' Days, ' + hours + ':' + minutes + ':' + seconds
 }
 
 function PageShell({ children }) {
